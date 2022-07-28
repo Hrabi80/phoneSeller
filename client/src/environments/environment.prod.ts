e2c7b33f32cd3593bf69e44f5de31361b5269818
Hrabi80 <Hrabi.ahmed88@gmail.com>
2022-07-28 14:37:57 +0100

    edit url for production in front

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api_url:'http://localhost:3000',
-  frontURL:'http://localhost:4200',
+  api_url:'http://102.219.178.126/:3000',
+  frontURL:'http://102.219.178.126/:4200',
   facebok_id: '430977672213037', 
   facebook_secret_key : '3edd3bb40d19fc0d642dee1a0798541b'
 };
